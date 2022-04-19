9572cc539d8bd30e785ca48a13e5e4843d78eb1f
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2022-04-19 18:14:23 +0900

    Time: 0 ms (100.00%), Space: 41.6 MB (20.86%) - LeetHub

--- 59-spiral-matrix-ii/59-spiral-matrix-ii.java
+++ 59-spiral-matrix-ii/59-spiral-matrix-ii.java
@@ -5,28 +5,28 @@ class Solution {
 		int cnt = 1;
 		int i = 0;
 		int j = 0;
-		int start = 0;
-		int end = n;
+		int start = 0; // 시작 index
+		int end = n; // 끝 index
 
-		while (cnt <= n * n) {
-			while (j < end ){
+		while (cnt <= n * n) { // cnt가 n*n까지 반복
+			while (j < end) { // 끝 index까지 cnt 1씩 증가하면서 저장
 				answer[i][j++] = cnt++;
 			}
+			i++; // i, j값 조절
 			j--;
-			i++;
-			while( i < end) {
+			while (i < end) { // 끝 index까지 cnt 1씩 증가하면서 저장
 				answer[i++][j] = cnt++;
 			}
-			i--;
-			while(j > start) {
+			i--; // i값 조절
+			while (j > start) { // 시작 index까지 cnt 1씩 증가하면서 저장
 				answer[i][--j] = cnt++;
 			}
-			start++;
+			start++; // 윗 줄과 옆 줄이 다 채워졌으므로 start +1, end -1
 			end--;
-			while(i > start) {
+			while (i > start) { // 시작 index까지 cnt 1씩 증가하면서 저장
 				answer[--i][j] = cnt++;
 			}
-			j++;
+			j++; // j값 조절
 		}
 
 		return answer;
