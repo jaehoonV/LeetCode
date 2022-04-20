bc0f320fec46ad66f96ffd746fcaadfb38f1dc16
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2022-04-20 19:28:07 +0900

    Time: 8 ms (40.84%), Space: 44.1 MB (43.53%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] s_arr = s.toCharArray();
-		char[] t_arr = t.toCharArray();
-		
-		if(s_arr.length != t_arr.length) return false;
-		
-		Arrays.sort(s_arr);
-		Arrays.sort(t_arr);
-
-		for (int i = 0; i < s_arr.length; i++)
-			if (s_arr[i] != t_arr[i])
+        if (s.length() != t.length()) {// 길이가 같지 않을 경우 false 반환
+			return false;
+		}
+		int[] count = new int[26]; // 알파벳 소문자 수 만큼 count할 배열 생성
+		for (int i = 0; i < s.length(); i++) {
+			count[s.charAt(i) - 'a']++; // s에 있는 문자는 +1
+			count[t.charAt(i) - 'a']--; // t에 있는 문자는 -1
+		}
+		// count배열에 0이 아닌 수가 있다면 s, t가 Anagram이 아닌 경우이므로 false 반환 
+		for (int i : count) { 
+			if (i != 0) {
 				return false;
-
+			}
+		}
 		return true;
     }
 }
