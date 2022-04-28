d17f44401b3ff597558c3c28253690dd5d7fe4af
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2022-04-28 15:38:39 +0900

    Time: 1 ms (95.78%), Space: 42.1 MB (95.75%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,12 +1,11 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> hm = new HashMap<>(); 
-        for(int n : nums) 
-            hm.put(n, hm.getOrDefault(n, 0) + 1);
+        //xor solution
+        int ans =0;
         
-        for(int key : hm.keySet()){
-            if(hm.get(key) == 1) return key;
-        } 
-        return 0;
+        for(int n : nums)
+        ans ^= n;
+    
+        return ans;
     }
 }
