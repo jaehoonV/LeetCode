638b572d423fb6ff137bc30152139dc4db74f1d7
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2022-05-28 11:28:20 +0900

    Time: 6 ms (26.16%), Space: 43 MB (86.05%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    // Time Limit Exceeded
+    // contains lambda 방식  Time Limit Exceeded
 //     public int missingNumber(int[] nums) {
         
 //         for(int i = 0; i <= nums.length; i++){
@@ -12,21 +12,36 @@ class Solution {
 //     public boolean contains(int[] nums, int key) { // Lambda
 //         return Arrays.stream(nums).anyMatch(i -> i == key);
 //     }
-    
-    public int missingNumber(int[] nums) {
+// ------------------------------------------------------------    
+    // sum 방식
+//     public int missingNumber(int[] nums) {
         
+//         Arrays.sort(nums);
+//         int n = nums[nums.length-1];
+        
+//         // 가우스 덧셈
+//         int n_sum = (1+n)*(n/2)+(n%2==0?0:(1+n)/2); 
+        
+//         // stream() method 
+//         int nums_sum = Arrays.stream(nums).parallel().reduce(0,(a,b)->  a + b);
+        
+//         if(n_sum - nums_sum == 0 && nums[0] == 0) return n+1;
+        
+//         return n_sum - nums_sum == 0 ? 0 : n_sum - nums_sum;
+//     }
+// ------------------------------------------------------------   
+    //binary search
+    public int missingNumber(int[] nums) { 
         Arrays.sort(nums);
-        int n = nums[nums.length-1];
+        int left = 0, right = nums.length, mid= (left + right)/2;
         
-        // 가우스 덧셈
-        int n_sum = (1+n)*(n/2)+(n%2==0?0:(1+n)/2); 
+        while(left<right){
+            mid = (left + right)/2;
+            if(nums[mid]>mid) right = mid;
+            else left = mid+1;
+        }
         
-        // stream() method 
-        int nums_sum = Arrays.stream(nums).parallel().reduce(0,(a,b)->  a + b);
-        
-        if(n_sum - nums_sum == 0 && nums[0] == 0) return n+1;
-        
-        return n_sum - nums_sum == 0 ? 0 : n_sum - nums_sum;
+    return left;
     }
     
 }
