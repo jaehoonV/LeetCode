d712a2e2baf5b06d8222380d80391865131d2c43
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2022-05-29 20:15:52 +0900

    Time: 399 ms (20.07%), Space: 42.8 MB (76.61%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -2,9 +2,8 @@ class Solution {
     public int maxProduct(String[] words) {
         int answer = 0;
 		
+		// 문자열의 문자중복을 제거한 배열 생성
 		String[] temp_words = new String[words.length];
-		
-		// 비교하기 위한 반복
 		for (int i = 0; i < words.length; i++) {
 			int n = words[i].length();
 			String[] word_arr1 = new String[n];
@@ -17,18 +16,18 @@ class Solution {
 		
 		for (int i = 0; i < temp_words.length - 1; i++) {
 			for (int j = i + 1; j < temp_words.length; j++) {
-				if(words[i].length() * words[j].length() > answer) {
-					if(solution(temp_words[i], temp_words[j])) {
-						answer = Math.max(answer, words[i].length() * words[j].length());
-					}
+				int temp = words[i].length() * words[j].length();
+				if(temp > answer && solution(temp_words[i], temp_words[j])) {
+					answer = Math.max(answer, temp);
 				}
 			}
 		}
 
 		return answer;
 	}
-
-	private boolean solution(String word_first, String word_second) {
+	
+	// 포함 문자가 있는지 판별
+	private boolean solution(String word_first, String word_second) { 
 
 		// String 배열 생성
 		String[] word_arr2 = new String[word_second.length()];
