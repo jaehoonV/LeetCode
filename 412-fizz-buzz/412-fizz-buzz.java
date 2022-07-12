07dabb749b05333cfdfc9c8d9b6707420371cc0e
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2022-07-12 20:42:55 +0900

    Time: 1 ms (100.00%), Space: 43 MB (99.23%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        
+        List<String> list = new ArrayList<>();
+        
+        for(int i = 1; i <= n; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                list.add("FizzBuzz");
+                continue;
+            }
+            if(i % 3 == 0){
+                list.add("Fizz");
+                continue;
+            }
+            if(i % 5 == 0){
+                list.add("Buzz");
+                continue;
+            }
+            list.add(String.valueOf(i));
+        }
+        return list;
+        
+    }
+}
