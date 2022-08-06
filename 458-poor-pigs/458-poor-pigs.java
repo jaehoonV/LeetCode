8804eda356605e020ae99eb6b999a8f2766a3bed
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2022-08-06 12:23:07 +0900

    Time: 0 ms (100.00%), Space: 41.2 MB (19.20%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        
+        if(buckets==1) return 0;
+        
+        int output = 1;
+        int base = minutesToTest / minutesToDie + 1;
+        int timer = base;
+        
+        while(buckets > timer) {
+            timer *= base;
+            output += 1;
+        }
+        
+        return output;
+    }
+}
