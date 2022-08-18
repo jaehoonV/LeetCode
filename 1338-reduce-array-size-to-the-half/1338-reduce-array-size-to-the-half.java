b35d56e6ea12a2abff037e889e2f7e0ec2fed265
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2022-08-18 23:21:38 +0900

    Time: 76 ms (68.53%), Space: 85.5 MB (61.83%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -8,17 +8,33 @@ class Solution {
         }
         int output = 0;
         int temp = 0;
-        // 리스트
-        List<Integer> listKeySet = new ArrayList<>(hm.keySet());
+        // 리스트 내림차순 정렬 런타임 157ms, 메모리 88mb
+//         List<Integer> listKeySet = new ArrayList<>(hm.keySet());
 
-        Collections.sort(listKeySet, (value1, value2) -> (hm.get(value2).compareTo(hm.get(value1))));
-		for(int key : listKeySet) {
-			temp += hm.get(key);
+//         Collections.sort(listKeySet, (value1, value2) -> (hm.get(value2).compareTo(hm.get(value1))));
+// 		for(int key : listKeySet) {
+// 			temp += hm.get(key);
+//             output++;
+//             if(temp >= half_leng){
+//                 return output;
+//             }
+// 		}
+        
+        // value값 정렬
+        List<Integer> list = new ArrayList<>();
+        for (int v : hm.values()) {
+            list.add(v);
+        }
+        Collections.sort(list, Collections.reverseOrder());
+
+        for(int l : list) {
+			temp += l;
             output++;
             if(temp >= half_leng){
                 return output;
             }
 		}
+        
         return 1;
     }
 }
