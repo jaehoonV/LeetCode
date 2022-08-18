f7e9d57d2915fae6e2abf60f69beeea7b4baaa4c
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2022-08-18 23:13:29 +0900

    Time: 157 ms (14.62%), Space: 88 MB (49.44%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        int half_leng = arr.length / 2;
+        
+        for(int n : arr) {
+            hm.put(n, hm.getOrDefault(n, 0) + 1);
+        }
+        int output = 0;
+        int temp = 0;
+        // 리스트
+        List<Integer> listKeySet = new ArrayList<>(hm.keySet());
+
+        Collections.sort(listKeySet, (value1, value2) -> (hm.get(value2).compareTo(hm.get(value1))));
+		for(int key : listKeySet) {
+			temp += hm.get(key);
+            output++;
+            if(temp >= half_leng){
+                return output;
+            }
+		}
+        return 1;
+    }
+}
