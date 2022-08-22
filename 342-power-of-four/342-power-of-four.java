59ab523ec82afe05e0beb750432544d1db767d21
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2022-08-22 12:43:12 +0900

    Time: 1 ms (100.00%), Space: 39.5 MB (94.48%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        Math.abs(n); // 절대값
+        
+        if(n == 1) return true;
+        if(n % 4 != 0) return false;
+        
+        return solution(n);
+    }
+    
+    public boolean solution(int n) {
+        if(n < 4 && n != 1) return false;
+        if(n == 1) return true;
+        if(n % 4 != 0) return false;
+        n /= 4;
+        return solution(n);
+    }
+}
