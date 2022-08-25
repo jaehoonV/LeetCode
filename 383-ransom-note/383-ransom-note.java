da67569849e5a9f5659116368733213a27a42b40
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2022-08-25 19:13:49 +0900

    Time: 3 ms (92.28%), Space: 45.5 MB (79.75%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -5,17 +5,25 @@ class Solution {
         if(r_length > m_length) return false;
         
         int[] temp = new int[26];
-        Arrays.fill(temp, 0);
         
-        for(int i = 0; i < r_length; i++){
-            temp[r.charAt(i) - 'a']++;
-        }
-        for(int j = 0; j < m_length; j++){
-            temp[m.charAt(j) - 'a']--;
-        }
+//         for(int i = 0; i < r_length; i++){
+//             temp[r.charAt(i) - 'a']++;
+//         }
+//         for(int j = 0; j < m_length; j++){
+//             temp[m.charAt(j) - 'a']--;
+//         }
         
-        for(int k = 0; k < temp.length; k++){
-            if(temp[k] > 0) return false;
+//         for(int k = 0; k < temp.length; k++){
+//             if(temp[k] > 0) return false;
+//         }
+        
+        for (int i = 0; i < m_length; i++) {
+            temp[m.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < r_length; i++) {
+            if(--temp[r.charAt(i) - 'a'] < 0) {
+                return false;
+            }
         }
         
         return true;
