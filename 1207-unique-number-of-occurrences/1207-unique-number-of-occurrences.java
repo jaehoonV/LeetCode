94bd775d08b41eed83f8bb2a83ae80ea38ae4f29
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2022-11-30 18:23:37 +0900

    Time: 6 ms (16.01%), Space: 42.1 MB (43.92%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        
+        for(int i = 0; i < arr.length; i++){
+            hm.put(arr[i], hm.getOrDefault(arr[i], 0) + 1);
+        }
+        
+        Map<Integer, Integer> cnt = new HashMap<>();
+        for (Integer v : hm.values()) {
+            cnt.put(v, cnt.getOrDefault(v, 0) + 1);
+        }
+  
+        for (Integer v : cnt.values()) {
+            if(v > 1) return false;
+        }
+        
+        return true;
+    }
+}
