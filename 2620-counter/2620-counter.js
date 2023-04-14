88aeb9b1473adc6276551d1cad2148430049274e
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2023-04-14 21:07:42 +0900

    Time: 57 ms (58.71%), Space: 41.7 MB (88.91%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let num = n - 1;
+    return function() {
+        return ++num;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
