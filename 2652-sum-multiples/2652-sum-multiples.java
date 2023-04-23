f57ecb5fcd368bfed383550831be8db35e030173
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2023-04-23 18:28:18 +0900

    Time: 4 ms (66.67%), Space: 39.7 MB (77.78%) - LeetHub

---
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int sumOfMultiples(int n) {
+        int output = 0;
+        
+        for(int i = 0; i <= n; i++){
+            if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0) output += i;
+        }
+        return output;
+    }
+}
