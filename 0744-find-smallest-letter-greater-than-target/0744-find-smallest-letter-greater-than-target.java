abfff025a243a71821f39e4f989cc364e4d400dc
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2023-06-09 20:04:02 +0900

    Time: 0 ms (100.00%), Space: 44.4 MB (18.33%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        char output = letters[0];
+        
+        for(char c : letters){
+            if(c > target){
+                return c;
+            }
+        }
+        
+        return output;
+    }
+}
