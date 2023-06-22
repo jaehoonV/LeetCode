faacb0b2c457594718d04290ff26772bd83cf223
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2023-06-22 20:02:41 +0900

    Time: 57 ms (65.41%), Space: 41.2 MB (98.15%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return{
+        toBe:(value)=>{
+            if(val!==value){
+                throw new Error("Not Equal")
+            }else{
+                return true
+            }
+        },
+        notToBe:(value)=>{
+            if(val===value){
+                throw new Error("Equal")
+            }
+            else{
+                return true
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
