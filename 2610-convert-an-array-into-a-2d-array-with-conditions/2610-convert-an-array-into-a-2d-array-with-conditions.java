6d8d4e471accf5ce555842285ad2642d5ce7480a
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2023-06-30 17:35:26 +0900

    Time: 5 ms (42.89%), Space: 44.3 MB (30.47%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        ArrayList<List<Integer>> output = new ArrayList<>();
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		for(int n : nums){
+			map.put(n, map.getOrDefault(n, 0) + 1);
+		}
+		Set<Integer> keySet = map.keySet();
+		
+		while(func(map)){
+			ArrayList<Integer> temp_arr = new ArrayList<>();
+			for(Integer key : keySet) {
+				if(map.get(key) > 0){
+					temp_arr.add(key);
+					map.put(key, map.get(key) - 1);
+				}
+			}
+			output.add(temp_arr);
+		}
+		return output;
+    }
+	
+	public boolean func(Map<Integer,Integer> map){
+        for(int key : map.keySet()){
+            if(map.get(key) >= 1) return true;
+        }
+        return false;
+    }
+}
