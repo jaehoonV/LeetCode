599b465ee856ffa6c37fd61858620ca3d362b557
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2023-09-13 18:32:39 +0900

    Time: 4 ms (36.83%), Space: 43.6 MB (64.56%) - LeetHub

---
+++ 0135-candy/0135-candy.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int candy(int[] ratings) {
+        int output = 0;
+        int n = ratings.length;
+        
+        int[] tempA = new int[n];
+        Arrays.fill(tempA,1);       
+        for(int i = 1; i < n; i++){
+            if(ratings[i - 1] < ratings[i]){
+                tempA[i] = tempA[i - 1] + 1;
+            }
+        }
+        
+        int[] tempB = new int[n];
+        Arrays.fill(tempB,1);       
+        for(int j = n - 2; j >= 0; j--){
+            if(ratings[j + 1] < ratings[j]){
+                tempB[j] = tempB[j + 1] + 1;
+            }
+        }
+        
+        for(int k = 0; k < n; k++){
+            output += Math.max(tempA[k], tempB[k]);
+        }
+        
+        return output;
+    }
+}
