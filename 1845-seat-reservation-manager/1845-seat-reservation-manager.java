e1002e110cf980f27192e85a8d7e27b3b566a87d
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2023-11-06 20:43:44 +0900

    Time: 79 ms (52.87%), Space: 85.5 MB (93.44%) - LeetHub

---
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.java
@@ -0,0 +1,18 @@
+class SeatManager {
+    PriorityQueue<Integer> pq;
+    
+    public SeatManager(int n) {
+        pq = new PriorityQueue<>();
+        for(int i = 1; i <= n; i++){
+            pq.offer(i);
+        }
+    }
+    
+    public int reserve() {
+        return pq.poll();
+    }
+    
+    public void unreserve(int seatNumber) {
+        pq.offer(seatNumber);
+    }
+}
