137637b7ec66ffccd6a7e92a85ef999ace341e57
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2024-01-22 20:46:45 +0900

    Time: 2 ms (97.74%), Space: 45.7 MB (23.87%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,20 +1,15 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int n = nums.length;
+        int n = nums.length, a = 0;
         int[] output = new int[n];
-        int a = 0;
+        
         for(int i = 0; i < n; i++){
-            if(output[nums[i] - 1] == 0){
-                output[nums[i] - 1] = nums[i];
-            } else{
-                a = nums[i];
-            }
+            if(output[nums[i] - 1] == 0)output[nums[i] - 1] = nums[i];
+            else a = nums[i];
         }
         
         for(int j = 0; j < n; j++){
-            if(output[j] == 0){
-                return new int[]{a, j + 1};
-            }
+            if(output[j] == 0) return new int[]{a, j + 1};
         }
         return new int[]{0, 1};
     }
