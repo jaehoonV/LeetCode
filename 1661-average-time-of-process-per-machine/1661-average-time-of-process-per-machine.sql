77cf8900568477ffb881fcc3547081e0f3499191
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2024-08-27 21:53:08 +0900

    Time: 437 ms (42.07%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -6,5 +6,6 @@ FROM Activity A1, Activity A2
 WHERE 
     A1.machine_id = A2.machine_id 
     AND A1.process_id = A2.process_id 
-    AND A1.timestamp < A2.timestamp 
+    AND A1.activity_type = 'start' 
+    AND A2.activity_type = 'end'
 GROUP BY A1.machine_id;
