9094f884079c53297154ef2b7b4b20cc6a32fa2f
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2024-11-22 20:21:31 +0900

    Time: 3 ms (98.02%), Space: 55.5 MB (38.13%) - LeetHub

---
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (m * n != original.length) return new int[0][0];
+
+        int [][] output = new int[m][n];
+        int a = 0;
+
+        for (int i = 0 ; i < m ; i++) {
+            for (int j = 0 ; j < n ; j++)
+                output[i][j] = original[a++];
+        }
+        return output;
+    }
+}
