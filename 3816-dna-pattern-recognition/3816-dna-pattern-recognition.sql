6da23957cbce9a45983a824d1c57274ef7388675
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2025-03-18 23:33:13 +0900

    Time: 551 ms (86.11%), Space: 0B (100%) - LeetHub

---
+++ 3816-dna-pattern-recognition/3816-dna-pattern-recognition.sql
@@ -0,0 +1,11 @@
+/* Write your PL/SQL query statement below */
+SELECT 
+    sample_id,
+    dna_sequence,
+    species,
+    CASE WHEN dna_sequence LIKE 'ATG%' THEN 1 ELSE 0 END AS has_start,
+    CASE WHEN SUBSTR(dna_sequence, LENGTH(dna_sequence) - 2) IN ('TAA', 'TAG', 'TGA') THEN 1 ELSE 0 END AS has_stop,
+    CASE WHEN dna_sequence LIKE '%ATAT%' THEN 1 ELSE 0 END AS has_atat,
+    CASE WHEN dna_sequence LIKE '%GGG%' THEN 1 ELSE 0 END AS has_ggg
+FROM Samples 
+ORDER BY sample_id
