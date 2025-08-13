ffab8df47558240fa5501cdd586523dc4a7d673a
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2025-08-13 20:05:55 +0900

    Time: 8 ms (84.41%), Space: 44 MB (83.75%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        Math.abs(n); // 절대값
+
+        if(n == 0) return false;
+        if(n == 1) return true;
+        if(n % 3 != 0) return false;
+
+        return solution(n);
+    }
+
+    public boolean solution(int n) {
+        if(n < 3 && n != 1) return false;
+        if(n == 1) return true;
+        if(n % 3 != 0) return false;
+        return solution(n / 3);
+    }
+}
