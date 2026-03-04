6098259c3400fe9a60689dbfc20bff0eedc3e6e6
jaehoon <92502363+jaehoonV@users.noreply.github.com>
2026-03-04 18:37:26 +0900

    Time: 3 ms (28.65%), Space: 47.1 MB (65.84%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -6,25 +6,22 @@ class Solution {
         
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(mat[i][j] == 1) output += func(mat, i, j);
+                if(mat[i][j] == 1) {
+                    int row_sum = 0, col_sum = 0;
+
+                    for(int a = 0; a < n; a++){
+                        row_sum += mat[i][a];
+                    }
+                    
+                    for(int b = 0; b < m; b++){
+                        col_sum += mat[b][j];
+                    }
+
+                    output += (row_sum == 1 && col_sum == 1) ? 1 : 0;
+                }
             }
         }
         return output;
     }
     
-    public int func(int[][] mat, int i, int j) {
-        int row_sum = 0, col_sum = 0;
-        int m = mat.length;
-        int n = mat[0].length;
-        
-        for(int a = 0; a < n; a++){
-            row_sum += mat[i][a];
-        }
-        
-        for(int b = 0; b < m; b++){
-            col_sum += mat[b][j];
-        }
-        
-        return row_sum == 1 && col_sum == 1 ? 1 : 0;
-    }
 }
